f4dba1357a45d025b77552c301febe2b266806e4
justinvdm <justinvderm@gmail.com>
2025-07-23 11:30:30 +0200

    progress

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,9 +1,10 @@
 import { RequestInfo } from "rwsdk/worker";
+import ExampleAccordion from "@/app/components/Accordion";
 
 export function HomePage({ ctx }: RequestInfo) {
   return (
     <div>
-      <p>YOLO</p>
+      <ExampleAccordion />
     </div>
   );
 }
